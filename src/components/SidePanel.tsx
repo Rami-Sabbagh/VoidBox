c9206d35dff5e6fb7e5e01619b34b6d1974cd9e9
Rami Sabbagh <ramilego4game@gmail.com>
2021-02-13 23:42:28 +0200

    Add a slider to set the simulation speed

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,12 +1,21 @@
-import { creatorOptions } from '../PixiApp';
+import { useCallback, useState } from 'react';
+import { creatorOptions, engine } from '../PixiApp';
 import CheckBox from './CheckBox';
+import Range from './Range';
 
 function toggleStatic() {
     creatorOptions.static = !creatorOptions.static;
 }
 
 export default function SidePanel() {
+    const [speed, setSpeed] = useState(1);
+    const onSpeedChange = useCallback((value) => {
+        setSpeed(value);
+        engine.timing.timeScale = value;
+    }, [setSpeed]);
+
     return (<div className="side-panel">
         <CheckBox id="spawnStatic" onToggle={toggleStatic}>Make object static</CheckBox>
+        <Range id="simulationSpeed" value={speed} min={0.01} max={2} step={0.01} onChange={onSpeedChange}>Simulation Speed ({speed})</Range>
     </div>);
 }

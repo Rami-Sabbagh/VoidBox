4e4bb0497ed57dad83ad95cc9518afe33cc2e224
Rami Sabbagh <ramilego4game@gmail.com>
2021-02-13 23:56:34 +0200

    Add a button for creating world boundries

--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from 'react';
-import { creatorOptions, engine } from '../PixiApp';
+import { creatorOptions, app, engine, spawnBox } from '../PixiApp';
 import CheckBox from './CheckBox';
+import Button from './Button';
 import Range from './Range';
 
 function toggleStatic() {
@@ -14,8 +15,23 @@ export default function SidePanel() {
         engine.timing.timeScale = value;
     }, [setSpeed]);
 
+    const [boundriesCreated, setBoundriesCreated] = useState(false);
+    const createBoundries = useCallback(() => {
+        const thickness = 5;
+        const { width, height } = app.screen;
+
+        spawnBox(width / 2, thickness, width, thickness * 2, true);
+        spawnBox(width / 2, height - thickness, width, thickness * 2, true);
+
+        spawnBox(thickness, height / 2, thickness * 2, height, true);
+        spawnBox(width - thickness, height / 2, thickness * 2, height, true)
+
+        setBoundriesCreated(true);
+    }, [setBoundriesCreated]);
+
     return (<div className="side-panel">
         <CheckBox id="spawnStatic" onToggle={toggleStatic}>Make object static</CheckBox>
         <Range id="simulationSpeed" value={speed} min={0.01} max={2} step={0.01} onChange={onSpeedChange}>Simulation Speed ({speed})</Range>
+        <Button disabled={boundriesCreated} onClick={createBoundries}>Create boundries</Button>
     </div>);
 }
